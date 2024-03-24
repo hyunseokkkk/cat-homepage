54eada8909a2d71ba9fb93549eb2dfc83e9f9ad3
aptheparker <apapap1119@g.skku.com>
2024-03-24 11:58:32 +0900

    1: html_web with html&css files separated

---
+++ 1_html_web/index.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<!-- 보기 불편해서 css 파일로 뺐습니다. -->
+		<link rel="stylesheet" href="./style.css">
+	</head>
+	<body>
+
+		<header>
+        <h1>GRAPE 고양이 웹</h1>
+    </header>
+
+		<form>
+
+			<label for="username">username</label>
+			<input name="username" type="text">
+
+			<label for="password">password</label>
+			<input name="password" type="password">
+
+			<input type="submit" value="로그인">
+
+		</form>
+
+		<footer>
+        <p>&copy; This is Cat Webpage. All rights reserved.</p>
+    </footer>
+
+	</body>
+</html>
